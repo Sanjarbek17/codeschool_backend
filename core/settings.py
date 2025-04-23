8433c61543581b79406ab57cb65214c5d5ef34c7
Sanjarbek17 <sanjarsaidov173@gmail.com>
2025-04-23 11:44:25 +0500

    Add static and media file configurations; update URL routing for Flutter web app

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,6 +120,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Add the static files directory for the Flutter web app
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'flutter_web/static'),
+]
+
+# Media files configuration
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- core/urls.py
+++ core/urls.py
@@ -14,10 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+from django.conf.urls.static import static
+
+from core import settings
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+FLUTTER_WEB_APP = os.path.join(BASE_DIR, 'flutter_web')
+
+def flutter_redirect(request, resource):
+    return serve(request, resource, FLUTTER_WEB_APP)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('editor/', include('editor.urls')),
-]
+    path('', lambda r: flutter_redirect(r, 'index.html')),
+    path('flutter_web/<path:resource>', flutter_redirect),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
