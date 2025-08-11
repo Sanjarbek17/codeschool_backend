e2e7f02c0b343933278dde0c89fb6001a69d22f8
Sanjarbek17 <sanjarsaidov173@gmail.com>
2025-08-11 06:55:34 +0500

    feat: Configure static files handling in settings and urls for development

--- core/urls.py
+++ core/urls.py
@@ -64,4 +64,9 @@ urlpatterns = [
     
     # Editor endpoints
     path("editor/", include("apps.editor.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# Serve static and media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
